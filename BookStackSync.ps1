64901dd5db2e8c685cc86d0a1ef143539b8a9fad
sirboffalot <d@sngr.uk>
2020-05-30 21:41:34 +0100

    Initial commit, adds modules and a sync script

---
+++ BookStackSync.ps1
@@ -0,0 +1,31 @@
+﻿# A simple script to export the content of all books in a BookStack wiki to a specified folder
+
+# Import the module
+Import-Module "$PSScriptRoot/Modules/BookStack.psm1" -Force
+
+# Variables
+$OutputFolder = "C:\mywikidumpfolder"
+$APIToken = ""
+$APISecret = ""
+$WikiURL = "https://my.wiki.url"
+$ExportFormat = "html"
+$CleanFolder = $false # CAREFUL!
+
+# Get the books
+$BookStackBooks = Get-BookStackBooks -URL $WikiURL -Token $APIToken -Secret $APISecret
+Write-Progress -PercentComplete 0 -Activity "Preparing" -Status "Starting the export $($BookStackBooks.Count) books to download.)"
+
+# Clean the directory?
+If($CleanFolder) 
+{ 
+    Write-Progress -PercentComplete 0 -Activity "Preparing" -Status "Cleaning folders"
+    Get-ChildItem -Path $OutputFolder | Remove-Item -Force 
+}
+
+# Export each book
+$ExportProgress = 0
+foreach($BookStackBook in $BookStackBooks) {
+    $ExportProgress++
+    Write-Progress -PercentComplete ((100/$BookStackBooks.Count) * $ExportProgress) -Activity "Exporting books" -Status ("Exporting " + $BookStackBook.name)
+    Export-BookStackBook -URL $WikiURL -Token $APIToken -Secret $APISecret -BookID $BookStackBook.id -OutputFormat $ExportFormat -OutputPath "$OutputFolder\$($BookStackBook.slug).$ExportFormat"
+}
---
+++ Modules/BookStack.psm1
@@ -0,0 +1,140 @@
+﻿<#
+.Synopsis
+   Get BookStack books using the API
+.DESCRIPTION
+   Retrieves a lost of Bookstack books using a BookStack API call
+.EXAMPLE
+   Get-BookStackBooks -URL "https://bookstack.local" -Token "1234567890" -Secret "0987654321"
+#>
+function Get-BookStackBooks
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([String])]
+    Param
+    (
+        # URL BookStack base url
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [String]
+        $URL,
+        
+        # Token BookStack API token
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=1)]
+        [String]
+        $Token,
+
+        # Secret BookStack API token secret
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=2)]
+        [String]
+        $Secret
+    )
+
+    Begin
+    {
+        
+    }
+    Process
+    {
+        $ApiURI = $URL + "/api/books"
+        $ApiHeaders = @{"Authorization" = "Token " + $Token + ':' + $Secret}
+        $BookStackBooks = Invoke-RestMethod -Uri $ApiURI -Headers $ApiHeaders
+    }
+    End
+    {
+        return $BookStackBooks.data
+    }
+}
+
+<#
+.Synopsis
+   Export a BookStack book
+.DESCRIPTION
+   Exports a BookStack book as HTML to the a specified location in a specified format
+.EXAMPLE
+   Export-BookStackBook -URL "https://bookstack.local" -Token "1234567890" -Secret "0987654321" -BookID 1 -OutputFormat "HTML" -OutputPath "C:\MyBooks\ThisBook.html"
+.EXAMPLE
+   Export-BookStackBook -URL "https://bookstack.local" -Token "1234567890" -Secret "0987654321" -BookID 2 -OutputFormat "PDF" -OutputPath "C:\MyBooks\ThisBook.pdf"
+#>
+function Export-BookStackBook
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([String])]
+    Param
+    (
+        # URL BookStack base url
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [String]
+        $URL,
+        
+        # Token BookStack API token
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=1)]
+        [String]
+        $Token,
+
+        # Secret BookStack API token secret
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=2)]
+        [String]
+        $Secret,
+
+        # BookID BookStack BookID to export
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=3)]
+        [int]
+        $BookID,
+
+        # OutputFormat Export format (PDF or HTML)
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=4)]
+        [ValidateSet(“PDF”, "HTML")] 
+        [String]
+        $OutputFormat,
+
+        # ExportPath Export path (including filename)
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=5)]
+        [String]
+        $OutputPath
+    )
+
+    Begin
+    {
+        
+    }
+    Process
+    {
+        # Build the request
+        Switch($OutputFormat.ToLower()) {
+            "PDF" {
+                $ApiURI = $URL + "/api/books/$BookID/export/pdf"
+            }
+            default {
+                $ApiURI = $URL + "/api/books/$BookID/export/html"
+            }
+        }
+
+        $ApiHeaders = @{"Authorization" = "Token " + $Token + ':' + $Secret}
+        $BookContent = Invoke-RestMethod -Uri $ApiURI -Headers $ApiHeaders
+    }
+    End
+    {
+        return ($BookContent | Out-File -FilePath $OutputPath -Force)
+    }
+}
+
+Export-ModuleMember -Function * 
